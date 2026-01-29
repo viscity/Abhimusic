a5c54f337ee4d8223124d4c0912af3e3d09ebe5f
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-01-29 11:13:44 +0530

    Update upstream repository and support channel links

--- config.py
+++ config.py
@@ -37,15 +37,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/viscity/zara.git",
+    "https://github.com/viscity/Abhimusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botscomplex")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+28QtimyLemZiZGVl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Thetiamusicbots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Lczyf4wpVGVmNDI1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
